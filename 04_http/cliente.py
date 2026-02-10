13cf7517ca5f108b91159986c0961345b921bf9a
cross0772 <andresesteb0772@gmail.com>
2026-02-10 14:03:07 -0500

    feat: se agregaron el contenido de los archivos

--- 04_http/cliente.py
+++ 04_http/cliente.py
@@ -1,2 +1,16 @@
 # Cliente
+import http.client
+import socket
 
+HOST = "localhost"
+PORT = 8080
+
+cliente = http.client.HTTPConnection(HOST, PORT)
+cliente.request("GET", "/")
+
+respuesta = cliente.getresponse()
+datos = respuesta.read().decode()
+
+print(datos)
+
+cliente.close()
--- 04_http/servidor.py
+++ 04_http/servidor.py
@@ -1,2 +1,14 @@
 # Servidor
+import http.server
+import socket
 
+HOST = "localhost"
+PORT = 8080
+
+
+class Servidor(http.server.SimpleHTTPRequestHandler):
+    pass
+
+
+servidor = http.server.HTTPServer((HOST, PORT), Servidor)
+servidor.serve_forever()
