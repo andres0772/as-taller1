16f8d238c00eac212309c0b7ee982b4a7bb02a3c
cross0772 <andresesteb0772@gmail.com>
2026-02-13 15:33:33 -0500

    se agrego funcionalidad del cliente

--- 05_proyecto/cliente.py
+++ 05_proyecto/cliente.py
@@ -1,2 +1,20 @@
-# Cliente
+import socket
 
+HOST = "localhost"
+PORT = 8080
+
+cliente = socket.socket()
+cliente.connect((HOST, PORT))
+
+print(cliente.recv(1024).decode())
+
+while True:
+    letras = input("digite la letra: ")
+    cliente.send(letras.encode())
+
+    respuesta = cliente.recv(1024).decode()
+    print(respuesta)
+
+    if "ganaste" in respuesta or "perdiste" in respuesta:
+        break
+cliente.close()
